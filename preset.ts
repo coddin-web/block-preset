28f889e517f9c5e86b00ed77a68fa14a691fc1b7
Falko Woudstra <falko@coddin.nl>
2024-08-15 17:17:11 +0200

    initial

--- preset.ts
+++ preset.ts
@@ -1,10 +1,46 @@
+import slugify from "slugify";
+import {editFiles} from "@preset/core";
+
 export default definePreset({
 	name: 'block-preset',
 	options: {
 		// ...
 	},
-	handler: async() => {
-		await extractTemplates()
-		// ...
+	handler: async({prompts}) => {
+		// ask Block name and slug
+		await prompt({ name: 'name', text: 'Block name?' });
+
+		await prompt({ name: 'slug', text: 'Block slug?', default: slugify(prompts.name, {lower: true, trim: true}) });
+
+		await extractTemplates({
+			to: 'blocks/' + prompts.slug,
+		});
+
+		await renamePaths({
+			paths: 'blocks/' + prompts.slug + '/slug.php',
+			transformer: prompts.slug + '.php',
+		});
+
+		await renamePaths({
+			paths: 'blocks/' + prompts.slug + '/slug.scss',
+			transformer: prompts.slug + '.scss',
+		});
+
+		await editFiles({
+			files: [
+				'blocks/' + prompts.slug + '/' + prompts.slug + '.php',
+				'blocks/' + prompts.slug + '/' + prompts.slug + '.scss',
+				'blocks/' + prompts.slug + '/block.json',
+			],
+			operations: [
+				{
+					type: 'replace-variables',
+					variables: {
+						'blockName': prompts.name,
+						'blockSlug': prompts.slug,
+					},
+				}
+			],
+		});
 	},
 })
---
+++ templates/slug.php
@@ -0,0 +1,3 @@
+<div class="@@blockSlug">
+    <!-- Silence is silver -->
+</div>
